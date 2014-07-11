c046af573f9bb0091330725202c59e9ee371aeac
taabi <taabi@taabi-macbookpro.roam.corp.google.com>
2014-07-11 15:16:55 -0700

    Initial Commit

---
+++ Blank/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Blank
+//
+//  Created by taabi on 7/11/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
